5e39b9ceacce79ca764c93c6f118ec493dbcd0b2
Senthilkumar Paramathma <ipsenthilkumar@gmail.com>
2019-12-12 17:17:25 -0500

    First commit

---
+++ react-app-kubernetes/build/precache-manifest.5fd28ae61645e6345647c39e911089de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15f47871042eb16a3ec5dc45326951a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb29c1770fcc6b4a41c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec95fa8aa8caa1de7629",
+    "url": "/static/js/2.48cdbeac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.48cdbeac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cb29c1770fcc6b4a41c",
+    "url": "/static/js/main.0b65f49e.chunk.js"
+  },
+  {
+    "revision": "f7b008f94e9460bacfe7",
+    "url": "/static/js/runtime-main.eb41b295.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
